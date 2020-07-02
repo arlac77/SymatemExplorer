cd41382373c859b9a40a96e76cff7b4328849f1b
Markus Felten <Markus.Felten@gmx.de>
2020-07-02 17:50:54 +0200

    fix: typos

--- src/main.mjs
+++ src/main.mjs
@@ -1,14 +1,9 @@
-import { BaseRouter, route, Guard } from "svelte-guard-history-router";
+import { BaseRouter, Guard } from "svelte-guard-history-router";
 import { Session } from "svelte-session-manager";
 import App from "./App.svelte";
 import base from "consts:base";
-import {
-  Diff,
-  SymbolInternals,
-  RustWasmBackend,
-  RelocationTable
-} from "SymatemJS";
-import { query_selector_all } from "svelte/internal";
+
+import { SymbolInternals, RustWasmBackend } from "SymatemJS";
 
 export const session = new Session(localStorage);
 
@@ -22,10 +17,9 @@ class SessionGuard extends Guard {
 
 const needsSession = new SessionGuard();
 
-export const router = new Router( [], base);
+export const router = new BaseRouter([], base);
 
-export async function fetchSymbols() {
-}
+export async function fetchSymbols() {}
 
 export async function fetchTriples() {
   const backend = await new RustWasmBackend();
