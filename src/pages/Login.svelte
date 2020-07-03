168d4bfd2e15b206e54547db886cad32ddc466b7
Markus Felten <Markus.Felten@gmx.de>
2020-07-03 22:46:34 +0200

    fix(deps): bump

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,19 +1,15 @@
 <script>
   import { Login } from "svelte-session-manager";
+  import { getContext } from "svelte";
+  import { ROUTER } from "svelte-guard-history-router";
   import { session } from "../main.mjs";
-  import api from 'consts:api';
-
-  export let state;
-
-  async function result() {
-    const transition = state.router.transition;
-
-    if (transition) {
-      transition.continue();
-    }
-  }
+  import api from "consts:api";
+  const router = getContext(ROUTER);
 </script>
 
 <div class="center">
-  <Login {session} endpoint={api + '/authenticate'} {result} />
+  <Login
+    {session}
+    endpoint={api + '/authenticate'}
+    result={() => router.continue()} />
 </div>
