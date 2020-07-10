07f5956f61e3a7367ea9b5e5703e83b978853d09
Markus Felten <Markus.Felten@gmx.de>
2020-07-10 16:16:08 +0200

    feat: drop area

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,9 +1,33 @@
-<script>
+<script>	
+	export function dragover (ev) {
+		ev.preventDefault();
+		ev.dataTransfer.dropEffect = 'move';
+	}
+		
+	export function drop (ev, new_g) {
+		ev.preventDefault();
+		console.log(ev.dataTransfer);
+		
+		/*
+		const i = ev.dataTransfer.getData("item");
+		const old_g = ev.dataTransfer.getData("group");
+		const item = groups[old_g].items.splice(i,1)[0];
+		groups[new_g].items.push(item);
+		$: groups = groups;
+		*/
+	}
 </script>
 
-<div>
+<style>
+.drop {
+  border: 5px solid blue;
+  width:  200px;
+  height: 100px;
+}
+</style>
 
-    Drop file here
-    <div>
+<div>
+    <div class="drop" on:dragover={dragover} on:drop={event => drop(event, g)>
+        Drop file here
     </div>
 </div>
