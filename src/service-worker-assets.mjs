77e363a1110a4068363b73554b0e850e943e3508
Markus Felten <Markus.Felten@gmx.de>
2020-07-25 12:47:59 +0200

    fix: ad more assets

--- src/service-worker-assets.mjs
+++ src/service-worker-assets.mjs
@@ -2,10 +2,11 @@ import version from "consts:version";
 
 export const ASSET_GENERATION = version;
 export const ASSETS = [
-    "favicon.ico",
-    "index.html",
-    "bundle.main.mjs",
-    "bundle.css",
-    "bundle.svelte.css"
-  ];
-  
+  "logo.svg",
+  "favicon.ico",
+  "index.html",
+  "bundle.main.mjs",
+  "bundle.css",
+  "bundle.svelte.css",
+  "backend.wasm"
+];
